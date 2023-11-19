d4e31002345368452938dceae49095411344fdb1
galirkil <galirkil@gmail.com>
2023-11-19 22:56:22 +0300

    Add status codes to create_gdrive_file view

--- api/views.py
+++ api/views.py
@@ -1,5 +1,6 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.status import HTTP_201_CREATED, HTTP_400_BAD_REQUEST
 
 from api.gdrive import create_file
 
@@ -14,11 +15,13 @@ def create_gdrive_file(request):
         file_name = request.data['name']
         file_content = request.data['data']
     except KeyError:
-        return Response({
+        content = {
             'message': 'You have to provide \'name\' and \'data\' parameters'
-        })
+        }
+        return Response(content, status=HTTP_400_BAD_REQUEST)
     else:
         create_file(file_name, file_content)
-        return Response({
+        content = {
             'message': f'File \'{file_name}.txt\' was created on google drive'
-        })
+        }
+        return Response(content, status=HTTP_201_CREATED)
